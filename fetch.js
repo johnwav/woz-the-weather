a2701b698892fdcc02aa5d112f4d581fb2afc8f0
johnwav <102820657+johnwav@users.noreply.github.com>
2022-08-11 12:26:58 +0100

    first commit

---
+++ fetch.js
@@ -0,0 +1,20 @@
+
+let weather = {
+    apiKey: "1f4783ad45dc6205ddc79b6a88a3a6f3",
+    fetchData: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + London 
+            + "&appid=" 
+            + this.appKey
+            )
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+    },
+
+    displayWeather: function(data){
+
+    }
+};
+
+
